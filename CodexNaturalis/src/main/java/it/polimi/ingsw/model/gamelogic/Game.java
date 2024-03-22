b9a0b7afc685b4350fc79f329099a75d55bc3f03
iugal <glaguardia23@gmail.com>
2024-03-22 18:29:06 +0100

    Created Game methods: getWinner() nextTurn() someoneHas20Points((

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/gamelogic/Game.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/gamelogic/Game.java
@@ -1,15 +1,86 @@
 package it.polimi.ingsw.model.gamelogic;
 import it.polimi.ingsw.model.gamecards.*;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
+
 public class Game{
-     private List<Player> ListOfPlayers;
-    private int numOfPlayers;
+    private List<Player> listOfPlayers=new ArrayList<>();
+    private final  int numOfPlayers;
     private List<ResourceCard> resourceCardDeck;
     private List<GoldCard> goldCardDeck;
     private List<Card> visibleCards;
-    private List<Goal> listOfGoal;
-    private Player currentPlayer;
+    private final List<Goal> listOfGoal;
+    private Player currentPlayer; 
+
+    public Game(Player firstPlayer, int numOfPlayers, List<ResourceCard> resourceCardDeck, List<GoldCard> goldCardDeck, List<Card> visibleCards, List<Goal> listOfGoal) {
+        listOfPlayers.add(firstPlayer);
+        this.numOfPlayers = numOfPlayers;
+        this.resourceCardDeck = resourceCardDeck;
+        this.goldCardDeck = goldCardDeck;
+        this.visibleCards = visibleCards;
+        this.listOfGoal = listOfGoal;
+    }
+
+    public List<Player> getPlayers() {
+        return new ArrayList<>(listOfPlayers);
+    }
+
+    public void addPlayer(Player p) {
+        listOfPlayers.add(p);
+    }
+
+    public int getNumOfPlayers() {
+        return numOfPlayers;
+    }
+
+    public List<ResourceCard> getResourceCardDeck() {
+        return resourceCardDeck;
+    }
+
+    public List<GoldCard> getGoldCardDeck() {
+        return goldCardDeck;
+    }
+
+    public List<Card> getVisibleCards() {
+        return visibleCards;
+    }
+
+    public List<Goal> getListOfGoal() {
+        return listOfGoal;
+    }
+
+    public void setCurrentPlayer(Player currentPlayer) {
+        this.currentPlayer = currentPlayer;
+    }
+
+    public Player getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    public void nextTurn(){
+        int currPlayerIdx= listOfPlayers.indexOf(currentPlayer);
+        Player nextPlayer= listOfPlayers.get((currPlayerIdx+1) % numOfPlayers); //if currPlayerIdx+1==numOfPlayers next player is the first of the list
+        setCurrentPlayer(nextPlayer);
+    }
+
+    public Optional<Player> getWinner(){
+
+        int maxPoints = listOfPlayers.stream().max(Comparator.comparing((Player::getPoints))).
+                        get().getPoints();
+        // don't know if a check for maxPoints >=20 is needed
+        return listOfPlayers.stream().
+                filter(p->p.getPoints() == maxPoints).
+                max(Comparator.comparing((Player::getGoalPoints)));
+
+    }                              
+
+    public boolean someoneHas20Points(){
+        return listOfPlayers.stream().anyMatch(p->p.getPoints()>=20);
+    }
+
 
 
 }
--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/gamelogic/Player.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/gamelogic/Player.java
@@ -11,11 +11,85 @@ public class Player {
     private Map<Coordinates,Card> field;
 
     private StarterCard starterCard;
-
+    private List<Coordinates> availablePositions;
     private String name;
     private String check;
     private int points;
+    private int goalPoints;
 
-    private Map<Resource,Integer> resourceCounters;
+    public Player(String name){
+        this.name = name;
+    }
 
+    public int getGoalPoints() {
+        return goalPoints;
+    }
+
+    public void setGoalPoints(int goalPoints) {
+        this.goalPoints = goalPoints;
+    }
+
+    public List<Card> getHand() {
+        return hand;
+    }
+
+    public void setHand(List<Card> hand) {
+        this.hand = hand;
+    }
+
+    public Goal getGoal() {
+        return goal;
+    }
+
+    public void setGoal(Goal goal) {
+        this.goal = goal;
+    }
+
+    public Map<Coordinates, Card> getField() {
+        return field;
+    }
+
+    public void setField(Map<Coordinates, Card> field) {
+        this.field = field;
+    }
+
+    public StarterCard getStarterCard() {
+        return starterCard;
+    }
+
+    public void setStarterCard(StarterCard starterCard) {
+        this.starterCard = starterCard;
+    }
+
+    public List<Coordinates> getAvailablePositions() {
+        return availablePositions;
+    }
+
+    public void setAvailablePositions(List<Coordinates> availablePositions) {
+        this.availablePositions = availablePositions;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCheck() {
+        return check;
+    }
+
+    public void setCheck(String check) {
+        this.check = check;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
 }
