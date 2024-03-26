ebc2b3239f1cf417e688c45e56582c14d6d1eb27
giolly <giorgio.mattina@gmail.com>
2024-03-26 10:49:39 +0100

    startGame()

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/gamelogic/Game.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/gamelogic/Game.java
@@ -17,6 +17,70 @@ public class Game{
     private List<Goal> listOfGoal;
     private Player currentPlayer;
 
+    private void startGame() {
+        //building players' hands
+        int i = 0;
+        int j = 0;
+        Coordinates origin = new Coordinates(0,0);
+        Player player;
+        //Mi manca un attimo come funziona APP ,cioè devo fare una copia dei mazzi da APP e poi fare lo shuffle
+
+        List<String> Colors= new ArrayList<String>();
+        Colors.add("Red");
+        Colors.add("Blue");
+        Colors.add("Yellow");
+        Colors.add("Green");
+
+
+        shuffle(resourceCardDeck);
+        shuffle(goldCardDeck);
+        shuffle(listOfGoal);
+
+
+        for (i = 0; i < numOfPlayers; i++) {
+
+            player = ListOfPlayers.get(i);
+            player.setname("Player"+i);
+            player.setColor(Colors.get(i));
+            player.setPoints(0);
+
+
+            //two resource cards
+            for (j = 0; j < 2; j++) {
+                player.addCardToHand(resourceCardDeck.removeFirst());
+
+            }
+
+            //one gold card
+            player.addCardToHand(goldCardDeck.removeFirst());
+
+
+            //one starter card
+            player.setStarterCard();
+            player.setGoal(listOfGoal.removeFirst());
+
+
+
+            //aggiungere posizione della starter card
+
+
+
+        }
+        //Builds a list of public goals
+
+        //Builds the visiblecardsdeck
+        for ( i=0 ; i< 2 ; i++) {
+            visibleCards.add(goldCardDeck.removeFirst());
+        }
+        for ( i=0 ; i< 2 ; i++) {
+            visibleCards.add(resourceCardDeck.removeFirst());
+        }
+
+
+
+    }
+
+}
     public void drawFromDeck(int choice) throws HandFullException {
         List<Card> hand = currentPlayer.getHand();
         if (hand.size() > 2) {
