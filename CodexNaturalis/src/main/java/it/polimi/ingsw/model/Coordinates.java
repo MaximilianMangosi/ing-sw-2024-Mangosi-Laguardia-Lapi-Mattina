e7e7a4e4cd971d6fb53ded73c7791d7405a0ef2e
iugal <glaguardia23@gmail.com>
2024-04-22 17:32:03 +0200

    override equals and hashCode in Coordinates.java

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/Coordinates.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/Coordinates.java
@@ -8,4 +8,17 @@ public class Coordinates {
         this.x = x;
         this.y = y;
     }
+    @Override
+    public boolean equals(Object other){
+        if(other==this)
+            return true;
+        if(other instanceof Coordinates temp)
+            return this.x==temp.x && this.y==temp.y;
+        return false;
+
+    }
+    @Override
+    public int hashCode(){
+        return (int)( 0.5*(x+y)*(x+y+1)+y);
+    }
 }
