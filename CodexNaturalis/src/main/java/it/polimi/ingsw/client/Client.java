6dd50acc6da643904af14c6be8ac0603bf3d1383
riclapi <ric.lapi@gmail.com>
2024-04-28 15:06:30 +0200

    Client

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/client/Client.java
@@ -0,0 +1,21 @@
+package it.polimi.ingsw.client;
+
+import it.polimi.ingsw.view.View;
+import it.polimi.ingsw.view.ViewInterface;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class Client {
+
+    public static void main(String[] args) {
+        try{
+
+            Registry registry = LocateRegistry.getRegistry();
+            ViewInterface view = (ViewInterface) registry.lookup("ViewRMI");
+
+        }catch (Exception error){
+            System.out.println(error.toString());
+        }
+    }
+}
