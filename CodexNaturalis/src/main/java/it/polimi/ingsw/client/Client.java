eb0285cb893166f54cbaeb9c193209346e319477
riclapi <ric.lapi@gmail.com>
2024-04-29 16:18:28 +0200

    implemented Client commands logic, javaDoc

--- CodexNaturalis/src/main/java/it/polimi/ingsw/client/Client.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/client/Client.java
@@ -3,6 +3,7 @@ package it.polimi.ingsw.client;
 import it.polimi.ingsw.controller.exceptions.*;
 import it.polimi.ingsw.model.Coordinates;
 import it.polimi.ingsw.model.gamecards.cards.Card;
+import it.polimi.ingsw.model.gamecards.exceptions.HandFullException;
 import it.polimi.ingsw.model.gamecards.exceptions.RequirementsNotMetException;
 import it.polimi.ingsw.model.gamecards.goals.Goal;
 import it.polimi.ingsw.model.gamelogic.exceptions.PlayerNameNotUniqueException;
@@ -19,6 +20,12 @@ import java.util.*;
 public class Client {
     private final String[] commands = {"start-game", "play-card", "choose-goal","choose-starter-card-side", "draw-card-from-deck", "draw-card-visible", "disconnect" };
     private UUID myUid;
+
+    /**
+     * @author Riccardo Lapi, Giuseppe Laguardia
+     * main function of the Client, connect to the server and listen to users commands
+     * @param args
+     */
     public static void main(String[] args) {
         Client client=new Client();
         /*
@@ -49,7 +56,8 @@ public class Client {
                         client.execCmd(s.nextLine().toLowerCase(Locale.ROOT),view);
                     } catch (UnacceptableNumOfPlayersException | InvalidUserId | InvalidGoalException |
                              PlayerNameNotUniqueException | IllegalOperationException | HandNotFullException |
-                             IsNotYourTurnException | RequirementsNotMetException | IllegalPositionException | InvalidCardException e) {
+                             IsNotYourTurnException | RequirementsNotMetException | IllegalPositionException |
+                             InvalidCardException | HandFullException | InvalidChoiceException | DeckEmptyException e) {
                         System.out.println(e.getMessage());
                     }
             }
@@ -58,7 +66,28 @@ public class Client {
         }
 
     }
-    private void execCmd(String cmd, ViewInterface view) throws UnacceptableNumOfPlayersException, PlayerNameNotUniqueException, RemoteException, IllegalOperationException, InvalidUserId, InvalidGoalException, HandNotFullException, IsNotYourTurnException, RequirementsNotMetException, IllegalPositionException, InvalidCardException {
+
+    /**
+     * @author Riccardo Lapi
+     * given the command the user want to execute, it asks the user for the parameters it needs to perform that operation
+     * @param cmd string that represent the command to use
+     * @param view the connected ViewInterface
+     * @throws UnacceptableNumOfPlayersException
+     * @throws PlayerNameNotUniqueException
+     * @throws RemoteException
+     * @throws IllegalOperationException
+     * @throws InvalidUserId
+     * @throws InvalidGoalException
+     * @throws HandNotFullException
+     * @throws IsNotYourTurnException
+     * @throws RequirementsNotMetException
+     * @throws IllegalPositionException
+     * @throws InvalidCardException
+     * @throws HandFullException
+     * @throws InvalidChoiceException
+     * @throws DeckEmptyException
+     */
+    private void execCmd(String cmd, ViewInterface view) throws UnacceptableNumOfPlayersException, PlayerNameNotUniqueException, RemoteException, IllegalOperationException, InvalidUserId, InvalidGoalException, HandNotFullException, IsNotYourTurnException, RequirementsNotMetException, IllegalPositionException, InvalidCardException, HandFullException, InvalidChoiceException, DeckEmptyException {
         Scanner s=new Scanner(System.in);
         switch (cmd){
             case "start-game":
@@ -81,7 +110,7 @@ public class Client {
                 System.out.println("Which card do you want to play? (1,2,3)");
                 int chosenCardI = s.nextInt();
                 Card chosenCard = view.showPlayerHand(myUid).get(chosenCardI - 1);
-                System.out.println("Which side? (f for front any for back)");
+                System.out.println("Which side? (f for front, b or any for back)");
                 boolean isChosenFront = s.nextLine().equals("f");
                 System.out.println("Where do you want to place the selected card [" + chosenCardI + "]? (int)");
                 int chosenPositionI = s.nextInt();
@@ -92,6 +121,27 @@ public class Client {
                 else view.playCardBack(chosenCard, chosenPosition, myUid);
                 break;
 
+            case "choose-starter-card-side":
+                System.out.println("Which side for the starter card? (f for front, b or any for back)");
+                boolean isChosenFrontStart = s.nextLine().equals("f");
+                view.chooseStarterCardSide(isChosenFrontStart, myUid);
+                break;
+
+            case "draw-card-from-deck":
+                System.out.println("What deck do you want to draw from? (0,1)");
+                int chosenDeck = s.nextInt();
+                view.drawFromDeck(myUid, chosenDeck);
+                break;
+
+            case "draw-card-visible":
+                System.out.println("Which card do you want to draw? (0, 1, 2, 3)");
+                int chosenDrawCard = s.nextInt();
+                view.drawVisibleCard(myUid,chosenDrawCard);
+                break;
+
+            case "disconnect":
+                view.closeGame(myUid);
+                break;
         }
     }
     private static void clearScreen () {
