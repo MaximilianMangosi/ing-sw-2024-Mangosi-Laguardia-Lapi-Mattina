c4486a93d44001d598afca1532c4fa112ccfb349
riclapi <ric.lapi@gmail.com>
2024-05-13 15:59:49 +0200

    start GUI

--- CodexNaturalis/src/main/java/it/polimi/ingsw/GUI/HelloApplication.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/GUI/HelloApplication.java
@@ -1,13 +1,33 @@
 package it.polimi.ingsw.GUI;
 
+import it.polimi.ingsw.client.GameData;
+import it.polimi.ingsw.client.ServerHandler;
+import it.polimi.ingsw.controller.exceptions.*;
+import it.polimi.ingsw.model.gamecards.exceptions.HandFullException;
+import it.polimi.ingsw.model.gamecards.exceptions.RequirementsNotMetException;
+import it.polimi.ingsw.model.gamelogic.exceptions.NoGameExistsException;
+import it.polimi.ingsw.model.gamelogic.exceptions.OnlyOneGameException;
+import it.polimi.ingsw.model.gamelogic.exceptions.PlayerNameNotUniqueException;
+import it.polimi.ingsw.model.gamelogic.exceptions.UnacceptableNumOfPlayersException;
+import it.polimi.ingsw.view.View;
+import it.polimi.ingsw.view.ViewRMIInterface;
+import it.polimi.ingsw.view.ViewSocket;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.Socket;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.UUID;
 
 public class HelloApplication extends Application {
+
+    static UUID myID;
+    static View view;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
@@ -19,5 +39,53 @@ public class HelloApplication extends Application {
 
     public static void main(String[] args) {
         launch();
+    }
+
+    public static void connectAndPlay(boolean isSocketSelected, String selectedUsername, int numOfPlayers) throws InvalidGoalException, HandFullException, InvalidChoiceException, IsNotYourTurnException, PlayerNameNotUniqueException, IOException, IllegalOperationException, InvalidCardException, DeckEmptyException, HandNotFullException, NoGameExistsException, InvalidUserId, RequirementsNotMetException, IllegalPositionException, ClassNotFoundException, UnacceptableNumOfPlayersException, OnlyOneGameException {
+
+        try {
+            GameData gameData = new GameData();
+            if (isSocketSelected) {
+                Socket server;
+                server = new Socket("192.168.0.1", 2323);
+                view = new ViewSocket(server.getOutputStream(), server.getInputStream(), gameData);
+                ServerHandler t1 = new ServerHandler((ViewSocket) view);
+            } else {
+                Registry registry = LocateRegistry.getRegistry(1099);
+                view = (ViewRMIInterface) registry.lookup("ViewRMI");
+            }
+        } catch (Exception e) {
+            //
+            return;
+        }
+
+        boolean error = true;
+
+        try {
+
+            myID = view.joinGame(selectedUsername);
+
+        } catch (PlayerNameNotUniqueException e) {
+            // handleNameNotUnique();
+        } catch (NoGameExistsException e) {
+            while (error) {
+                try {
+                    myID = view.bootGame(numOfPlayers, selectedUsername);
+                    error = false;
+                } catch (UnacceptableNumOfPlayersException ex1) {
+                    //
+                } catch (OnlyOneGameException ex) {
+                    //
+                    try {
+                        view.joinGame(selectedUsername);
+                        error = false;
+                    } catch (PlayerNameNotUniqueException e1) {
+                        // error = handleNameNotUnique();
+                    } catch (NoGameExistsException ignore) {
+                    }
+                }
+            }
+        }
+
     }
 }
--- CodexNaturalis/src/main/java/it/polimi/ingsw/GUI/HelloController.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/GUI/HelloController.java
@@ -1,14 +1,74 @@
 package it.polimi.ingsw.GUI;
 
+import it.polimi.ingsw.client.GameData;
+import it.polimi.ingsw.client.ServerHandler;
+import it.polimi.ingsw.controller.exceptions.*;
+import it.polimi.ingsw.model.gamecards.exceptions.HandFullException;
+import it.polimi.ingsw.model.gamecards.exceptions.RequirementsNotMetException;
+import it.polimi.ingsw.model.gamelogic.exceptions.NoGameExistsException;
+import it.polimi.ingsw.model.gamelogic.exceptions.OnlyOneGameException;
+import it.polimi.ingsw.model.gamelogic.exceptions.PlayerNameNotUniqueException;
+import it.polimi.ingsw.model.gamelogic.exceptions.UnacceptableNumOfPlayersException;
+import it.polimi.ingsw.view.View;
+import it.polimi.ingsw.view.ViewRMIInterface;
+import it.polimi.ingsw.view.ViewSocket;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.UUID;
 
 public class HelloController {
+    private boolean isSocketSelected = true;
+
+    private String selectedUsername;
+
+    private int numOfPlayers = 2;
     @FXML
     private Label welcomeText;
+
+    @FXML
+    private TextField usernameField;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+    @FXML
+    private void onSocketSelected(){
+        isSocketSelected = true;
+    }
+
+    @FXML
+    private void onRmiSelected(){
+        isSocketSelected = false;
+    }
+
+    @FXML
+    private void onUsernameChange(){
+        selectedUsername = usernameField.getText();
+    }
+
+    @FXML
+    private void onSelect2Player(){
+        numOfPlayers = 2;
+    }
+    @FXML
+    private void onSelect3Player(){
+        numOfPlayers = 3;
+    }
+    @FXML
+    private void onSelect4Player(){
+        numOfPlayers = 4;
+    }
+    @FXML
+    private void onPlay() throws InvalidGoalException, HandFullException, InvalidChoiceException, IsNotYourTurnException, UnacceptableNumOfPlayersException, OnlyOneGameException, PlayerNameNotUniqueException, IOException, IllegalOperationException, InvalidCardException, DeckEmptyException, HandNotFullException, NoGameExistsException, InvalidUserId, RequirementsNotMetException, IllegalPositionException, ClassNotFoundException {
+        HelloApplication.connectAndPlay(isSocketSelected, selectedUsername, numOfPlayers);
+    }
+
 }
