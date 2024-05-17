91f2fdeca87f12f4fbcfa40052d77f04037165ac
Gio-lly <giorgio.mattina@gmail.com>
2024-05-17 15:43:35 +0200

    Choose starter card controller etc

--- CodexNaturalis/src/main/java/it/polimi/ingsw/GUI/ChooseSideController.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/GUI/ChooseSideController.java
@@ -29,9 +29,40 @@ public class ChooseSideController {
     ImageView backImage;
     @FXML
     HBox handBox;
+    @FXML
+    ImageView privateGoal1;
+    @FXML
+    ImageView privateGoal2;
+    @FXML
+    ImageView publicGoal1;
+    @FXML
+    ImageView publicGoal2;
 
     ImageView[] handImage=new ImageView[3];
+    public void initialize() throws InvalidUserId, RemoteException {
+        int id=view.showPlayerGoalOptions()[0].getId();
+        Image privateOption1 = new Image(getClass().getResourceAsStream("src/main/resources/CardsFront/Goal/0"+id));
+        privateGoal1.setImage(privateOption1);
 
+         id=view.showPlayerGoalOptions()[1].getId();
+        Image privateOption2 = new Image(getClass().getResourceAsStream("src/main/resources/CardsFront/Goal/0"+id));
+        privateGoal1.setImage(privateOption2);
+
+        id=view.getPublicGoals()[0].getId();
+        Image publicOption1 = new Image(getClass().getResourceAsStream("src/main/resources/CardsFront/Goal/0"+id));
+        privateGoal1.setImage(publicOption1);
+        //TODO fix index of goal cards
+        id=view.getPublicGoals()[1].getId();
+        Image publicOption2  = new Image(getClass().getResourceAsStream("src/main/resources/CardsFront/Goal/0"+id));
+        privateGoal1.setImage(publicOption2);
+
+        id=view.getStarterCard(myID).getId();
+        Image starterCardFr  = new Image(getClass().getResourceAsStream("src/main/resources/CardsFront/StarterCards/0"+id));
+        frontImage.setImage(starterCardFr);
+
+        Image starterCardBk  = new Image(getClass().getResourceAsStream("src/main/resources/CardsBack/StarterCards/0"+id));
+        backImage.setImage(starterCardBk);
+    }
     public void setImageView(Image front,Image back){
         frontImage.setImage(front);
         backImage.setImage(back);
@@ -48,6 +79,13 @@ public class ChooseSideController {
         backImage.setLayoutX(500);
         backImage.setLayoutY(250);
 
+    }
+    @FXML
+    public void choosePrivateGoal1() throws InvalidUserId, IOException, InvalidGoalException, HandFullException, InvalidChoiceException, IsNotYourTurnException, UnacceptableNumOfPlayersException, OnlyOneGameException, PlayerNameNotUniqueException, IllegalOperationException, InvalidCardException, DeckEmptyException, HandNotFullException, NoGameExistsException, RequirementsNotMetException, IllegalPositionException, ClassNotFoundException {
+        view.chooseGoal(myID,view.showPlayerGoalOptions(myID)[0]);
+    }
+    public void choosePrivateGoal2() throws InvalidUserId, IOException, InvalidGoalException, HandFullException, InvalidChoiceException, IsNotYourTurnException, UnacceptableNumOfPlayersException, OnlyOneGameException, PlayerNameNotUniqueException, IllegalOperationException, InvalidCardException, DeckEmptyException, HandNotFullException, NoGameExistsException, RequirementsNotMetException, IllegalPositionException, ClassNotFoundException {
+        view.chooseGoal(myID,view.showPlayerGoalOptions(myID)[1]);
     }
     public void slideUpHand(){
         handBox.setLayoutY(590);
