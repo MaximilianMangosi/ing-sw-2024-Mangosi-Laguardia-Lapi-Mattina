7c224c7525a88b496891754aab4e9108a38cc55d
MaximiliamMangosi <145591559+MaximiliamMangosi@users.noreply.github.com>
2024-05-19 10:17:18 +0200

    added GUIController

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/GUI/GUIController.java
@@ -0,0 +1,39 @@
+package it.polimi.ingsw.GUI;
+
+import it.polimi.ingsw.view.View;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.util.UUID;
+
+public class GUIController {
+    View view;
+    UUID myID;
+    String myName;
+    public void setView(View view){
+        this.view = view;
+    }
+    public void setMyId(UUID myId){
+        this.myID = myId;
+    }
+    public void setMyName(String myName){
+        this.myName = myName;
+    }
+    public void changeScene(String pathXML, ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(pathXML));
+        Parent root = loader.load();
+        GUIController c = loader.getController();
+        c.setView(view);
+        c.setMyId(myID);
+        c.setMyName(myName);
+        Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
