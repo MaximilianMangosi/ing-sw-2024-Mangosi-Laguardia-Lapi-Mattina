30a088c09bd12198cb4d2b7a550f997392f49905
riclapi <ric.lapi@gmail.com>
2024-05-19 18:28:19 +0200

    start game controller

--- CodexNaturalis/src/main/java/it/polimi/ingsw/GUI/InGameController.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/GUI/InGameController.java
@@ -2,22 +2,32 @@ package it.polimi.ingsw.GUI;
 
 import it.polimi.ingsw.controller.exceptions.InvalidUserId;
 import it.polimi.ingsw.model.gamecards.cards.Card;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.*;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 
 import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InGameController extends GUIController {
     @FXML
     private HBox playerListBox=new HBox();
+
+    @FXML
+    private ImageView visibleCard1;
+    @FXML
+    private ImageView visibleCard2;
+    @FXML
+    private ImageView visibleCard3;
+    @FXML
+    private ImageView visibleCard4;
+
     private HBox handBox= new HBox();
     public void init() throws RemoteException, InvalidUserId {
         for (String p : view.getPlayersList()) {
@@ -36,6 +46,43 @@ public class InGameController extends GUIController {
             ImageView cardView = new ImageView(cardPng);
             handBox.getChildren().add(cardView);
         }
+
+    }
+
+    private void checkGameInfo(){
+
+        new Thread(() -> {
+
+            List<Card> oldCards = new ArrayList<>();
+            while(true){
+
+                //visible cards
+                try {
+                    if(!view.getVisibleCards().equals(oldCards)){
+                        Platform.runLater(() -> {
+                            try {
+                                updateVisibleCards(view.getVisibleCards());
+                            } catch (RemoteException e) {
+                                throw new RuntimeException(e);
+                            }
+                        });
+
+                        oldCards = view.getVisibleCards();
+                    }
+                } catch (RemoteException e) {
+                    throw new RuntimeException(e);
+                }
+
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
+    }
+
+    private void updateVisibleCards(List<Card> newCards){
 
     }
 }
