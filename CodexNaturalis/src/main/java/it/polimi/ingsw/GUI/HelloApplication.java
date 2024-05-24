40bf89c6bc09e3aceda404b0ccf6520984b75128
Gio-lly <giorgio.mattina@gmail.com>
2024-05-24 10:19:34 +0200

    .

--- CodexNaturalis/src/main/java/it/polimi/ingsw/GUI/HelloApplication.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/GUI/HelloApplication.java
@@ -26,6 +26,7 @@ import java.util.UUID;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 720, 600);
         stage.setTitle("Codex Naturalis");
